20385936698348718269a10c16235d7114213ede
Student P_03 <studentp_03@Student-P-03.local>
2016-08-09 16:16:12 +0530

    Initial Commit

---
+++ Music Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music Player
+//
+//  Created by Student P_03 on 09/08/16.
+//  Copyright © 2016 Akshay Awtade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Music Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Music Player
+//
+//  Created by Student P_03 on 09/08/16.
+//  Copyright © 2016 Akshay Awtade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
